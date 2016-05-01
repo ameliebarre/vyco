b994a05a3b73d89942413c4de77c0f0d9cb2900c
Amélie <amliebarre@gmail.com>
2016-05-02 00:26:01 +0200

    custom login templates authent

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'index' => 'home#index'
   get 'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
+  get 'login'   => 'sessions#new'
+  post 'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 end
